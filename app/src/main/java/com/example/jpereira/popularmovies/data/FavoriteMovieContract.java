b5926782b34d68dc9cbb33698bee89dcb13d31a6
José Batista Pereira <jpereira@Jose-MacBook.local>
2017-04-21 08:29:53 -0300

    feat: Create provider

--- app/src/main/java/com/example/jpereira/popularmovies/data/FavoriteMovieContract.java
+++ app/src/main/java/com/example/jpereira/popularmovies/data/FavoriteMovieContract.java
@@ -1,6 +1,10 @@
 package com.example.jpereira.popularmovies.data;
 
+import android.content.res.Resources;
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import com.example.jpereira.popularmovies.R;
 
 /**
  * Created by jpereira on 19/04/17.
@@ -11,7 +15,16 @@ public class FavoriteMovieContract {
     public FavoriteMovieContract() {
     }
 
+    public static final String CONTENT_AUTHORITY = Resources.getSystem().getString(R.string.content_authority);
+    public static final String PATH_MOVIE = "movie";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+
     public static class FavoriteMovieEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon()
+                .appendPath(PATH_MOVIE)
+                .build();
 
         public static final String TABLE_NAME = "favorite_movie";
 
---
+++ app/src/main/java/com/example/jpereira/popularmovies/data/PopularMovieProvider.java
@@ -0,0 +1,142 @@
+package com.example.jpereira.popularmovies.data;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jpereira on 19/04/17.
+ */
+
+public class PopularMovieProvider extends ContentProvider {
+
+    public static final int CODE_MOVIE = 100;
+    public static final int CODE_MOVIE_WITH_DATE = 101;
+
+    private FavoriteMovieDbHelper dbHelper;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    public static UriMatcher buildUriMatcher() {
+
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = FavoriteMovieContract.CONTENT_AUTHORITY;
+
+        // content://com.example.jpereira.popularmovies/movie
+        matcher.addURI(authority, FavoriteMovieContract.PATH_MOVIE, CODE_MOVIE);
+
+        // content://com.example.jpereira.popularmovies/movie/12
+        // /# is a number
+        matcher.addURI(authority, FavoriteMovieContract.PATH_MOVIE + "/#", CODE_MOVIE_WITH_DATE);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        dbHelper = new FavoriteMovieDbHelper(getContext());
+
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        Cursor cursor;
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE_WITH_DATE:
+                String id = uri.getLastPathSegment();
+                String[] selectionArguments = new String[]{id};
+                cursor = dbHelper.getReadableDatabase().query(
+                        FavoriteMovieContract.FavoriteMovieEntry.TABLE_NAME,
+                        projection,
+                        FavoriteMovieContract.FavoriteMovieEntry.MOVIE_ID + " = ? ",
+                        selectionArguments,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case CODE_MOVIE:
+                cursor = dbHelper.getReadableDatabase().query(
+                        FavoriteMovieContract.FavoriteMovieEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+
+        return cursor;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        throw new RuntimeException("Not implemented in this app");
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+
+        final SQLiteDatabase db = dbHelper.getWritableDatabase();
+        switch (sUriMatcher.match(uri)) {
+
+            case CODE_MOVIE:
+                try {
+                    db.beginTransaction();
+
+
+                    db.setTransactionSuccessful();
+                } finally {
+                    db.endTransaction();
+                }
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        int numRowsDeleted = 0;
+        if (selection == null) {
+            selection = "1";
+        }
+
+        switch (sUriMatcher.match(uri)) {
+            case CODE_MOVIE:
+                numRowsDeleted = dbHelper.getWritableDatabase().delete(
+                        FavoriteMovieContract.FavoriteMovieEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+
+        if (numRowsDeleted != 0) {
+            getContext().getContentResolver().notifyChange(uri, null);
+        }
+
+        return numRowsDeleted;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        throw new RuntimeException("Not Implemented in this app");
+    }
+}
